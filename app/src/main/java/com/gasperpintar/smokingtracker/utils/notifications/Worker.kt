57348e47fc4427a5daf327d89d974794c9c56154
Gašper Pintar <pintargasper1@gmail.com>
2025-11-05 01:39:59 +0100

    Worker update

--- app/src/main/java/com/gasperpintar/smokingtracker/utils/notifications/Worker.kt
+++ app/src/main/java/com/gasperpintar/smokingtracker/utils/notifications/Worker.kt
@@ -3,7 +3,7 @@ package com.gasperpintar.smokingtracker.utils.notifications
 import android.Manifest
 import android.content.Context
 import androidx.annotation.RequiresPermission
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.gasperpintar.smokingtracker.R
 import com.gasperpintar.smokingtracker.database.AppDatabase
@@ -11,7 +11,6 @@ import com.gasperpintar.smokingtracker.database.Provider
 import com.gasperpintar.smokingtracker.database.entity.HistoryEntity
 import com.gasperpintar.smokingtracker.database.entity.SettingsEntity
 import com.gasperpintar.smokingtracker.utils.Helper
-import kotlinx.coroutines.runBlocking
 import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -19,35 +18,36 @@ import java.time.LocalDateTime
 class Worker(
     context: Context,
     workerParams: WorkerParameters
-) : Worker(context, workerParams) {
+) : CoroutineWorker(context, workerParams) {
 
     private val database: AppDatabase by lazy { Provider.getDatabase(applicationContext) }
 
     @RequiresPermission(value = Manifest.permission.POST_NOTIFICATIONS)
-    override fun doWork(): Result {
+    override suspend fun doWork(): Result {
         Notifications.createNotificationChannel(applicationContext)
 
-        runBlocking {
-            val (_, endOfDay) = Helper.getDay(date = LocalDate.now())
-            val lastHistory: HistoryEntity? = database.historyDao().getLastHistoryEntry(endOfToday = endOfDay)
-            val settings: SettingsEntity? = database.settingsDao().getSettings()
+        val (_, endOfDay) = Helper.getDay(date = LocalDate.now())
+        val lastHistory: HistoryEntity? = database.historyDao().getLastHistoryEntry(endOfToday = endOfDay)
+        val settings: SettingsEntity? = database.settingsDao().getSettings()
 
-            val timeSinceLastCigarette = lastHistory?.let {
-                Duration.between(it.createdAt, LocalDateTime.now())
-            }
+        val timeSinceLastCigarette = lastHistory?.let {
+            Duration.between(it.createdAt, LocalDateTime.now())
+        }
 
-            if (timeSinceLastCigarette != null && timeSinceLastCigarette > Duration.ofMinutes(45) && settings?.notifications == 1) {
-                val timeString = timeSinceLastCigarette.toReadableString()
+        if (timeSinceLastCigarette != null &&
+            timeSinceLastCigarette > Duration.ofMinutes(45) &&
+            settings?.notifications == 1
+        ) {
+            val timeString = timeSinceLastCigarette.toReadableString()
 
-                Notifications.sendNotification(
-                    context = applicationContext,
-                    title = applicationContext.getString(R.string.notification_title),
-                    content = applicationContext.getString(
-                        R.string.notification_content,
-                        timeString
-                    )
+            Notifications.sendNotification(
+                context = applicationContext,
+                title = applicationContext.getString(R.string.notification_title),
+                content = applicationContext.getString(
+                    R.string.notification_content,
+                    timeString
                 )
-            }
+            )
         }
         return Result.success()
     }
