c74c3e26cda927d5dbfa4f96f01947feeb884a03
Gašper Pintar <pintargasper1@gmail.com>
2026-01-30 18:06:28 +0100

    Add images for achievements

--- app/src/main/java/com/gasperpintar/smokingtracker/type/AchievementIcon.kt
+++ app/src/main/java/com/gasperpintar/smokingtracker/type/AchievementIcon.kt
@@ -5,6 +5,17 @@ import com.gasperpintar.smokingtracker.R
 enum class AchievementIcon(
     val drawableResource: Int
 ) {
-    LENT(R.drawable.ic_lent),
-    EDIT(R.drawable.ic_edit)
+    W20H(R.drawable.ic_20h),
+    W30H(R.drawable.ic_30h),
+    W2D(R.drawable.ic_2d),
+    W60H(R.drawable.ic_60h),
+    W5D(R.drawable.ic_5d),
+    W150H(R.drawable.ic_150h),
+    W1W(R.drawable.ic_1w),
+    C50(R.drawable.ic_50c),
+    C100(R.drawable.ic_100c),
+    C150(R.drawable.ic_150c),
+    C200(R.drawable.ic_200c),
+    C250(R.drawable.ic_250c),
+    C300(R.drawable.ic_300c)
 }
