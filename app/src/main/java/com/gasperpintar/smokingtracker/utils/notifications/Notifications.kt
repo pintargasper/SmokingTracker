113576eab27c0b7c3c736848e52188b3853d6cc6
Gašper Pintar <pintargasper1@gmail.com>
2026-09-10 09:30:15 +0200

    Clean up Notifications

--- app/src/main/java/com/gasperpintar/smokingtracker/utils/notifications/Notifications.kt
+++ app/src/main/java/com/gasperpintar/smokingtracker/utils/notifications/Notifications.kt
@@ -14,6 +14,7 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import com.gasperpintar.smokingtracker.MainActivity
 import com.gasperpintar.smokingtracker.R
+import androidx.core.content.getSystemService
 
 object Notifications {
 
@@ -21,19 +22,11 @@ object Notifications {
     private const val CHANNEL_NAME = "Smoking Tracker Notifications"
     private const val CHANNEL_DESCRIPTION = "Smoking monitoring notifications"
 
-    fun createNotificationChannel(
-        context: Context
-    ) {
-        val channel = NotificationChannel(
-            CHANNEL_ID,
-            CHANNEL_NAME,
-            NotificationManager.IMPORTANCE_DEFAULT
-        ).apply {
+    fun createNotificationChannel(context: Context) {
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
             description = CHANNEL_DESCRIPTION
         }
-        val notificationManager: NotificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+        context.getSystemService<NotificationManager>()?.createNotificationChannel(channel)
     }
 
     fun sendNotification(
@@ -45,37 +38,35 @@ object Notifications {
     ) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
             ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS)
-            != PackageManager.PERMISSION_GRANTED) {
-            return
-        }
+            != PackageManager.PERMISSION_GRANTED
+        ) return
 
-        val intent: Intent = if (fileUri != null) {
+        val intent = fileUri?.let {
             Intent(Intent.ACTION_VIEW).apply {
-                setDataAndType(fileUri, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                setDataAndType(it, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
                 flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_ACTIVITY_NEW_TASK
             }
-        } else {
-            Intent(context, MainActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            }
+        } ?: Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
 
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+        val pendingIntent = PendingIntent.getActivity(
             context,
             0,
             intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
-        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_name)
-            .setContentTitle(title)
-            .setContentText(content)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-            .build()
-
-        NotificationManagerCompat.from(context).notify(notificationId, notification)
+        NotificationManagerCompat.from(context).notify(
+            notificationId,
+            NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build()
+        )
     }
 }
--- app/src/main/java/com/gasperpintar/smokingtracker/utils/notifications/Worker.kt
+++ app/src/main/java/com/gasperpintar/smokingtracker/utils/notifications/Worker.kt
@@ -6,13 +6,8 @@ import android.content.SharedPreferences
 import androidx.core.content.edit
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.gasperpintar.smokingtracker.Application
 import com.gasperpintar.smokingtracker.R
-import com.gasperpintar.smokingtracker.database.AppDatabase
-import com.gasperpintar.smokingtracker.database.Provider
-import com.gasperpintar.smokingtracker.repository.AchievementRepository
-import com.gasperpintar.smokingtracker.repository.HistoryRepository
-import com.gasperpintar.smokingtracker.repository.NotificationsSettingsRepository
-import com.gasperpintar.smokingtracker.repository.SettingsRepository
 import com.gasperpintar.smokingtracker.ui.fragment.achievements.AchievementEvaluator
 import com.gasperpintar.smokingtracker.utils.TimeHelper
 import java.time.Duration
@@ -26,86 +21,62 @@ class Worker(
     workerParams
 ) {
 
-    private val database: AppDatabase by lazy {
-        Provider.getDatabase(applicationContext)
-    }
-
-    private val historyRepository: HistoryRepository by lazy {
-        HistoryRepository(historyDao = database.historyDao())
-    }
-
-    private val achievementRepository: AchievementRepository by lazy {
-        AchievementRepository(achievementDao = database.achievementDao())
-    }
-
-    private val notificationsSettingsRepository: NotificationsSettingsRepository by lazy {
-        NotificationsSettingsRepository(notificationsSettingsDao = database.notificationsSettingsDao())
-    }
-
-    private val settingsRepository: SettingsRepository by lazy {
-        SettingsRepository(settingsDao = database.settingsDao())
+    private val container by lazy {
+        (applicationContext as Application).container
     }
 
     private val sharedPreferences: SharedPreferences by lazy {
         applicationContext.getSharedPreferences("settings", MODE_PRIVATE)
     }
 
+    @Override
     override suspend fun doWork(): Result {
+        val now = LocalDateTime.now()
 
-        val now: LocalDateTime = LocalDateTime.now()
-
-        val lastHistory = historyRepository.getLast()
-        val settings = settingsRepository.get()
-        val notifications = notificationsSettingsRepository.get()
+        val lastHistory = container.historyRepository.getLast()
+        val settings = container.settingsRepository.get()
+        val notifications = container.notificationsSettingsRepository.get()
 
         if (lastHistory != null) {
-            val achievementEvaluator = AchievementEvaluator(
+            AchievementEvaluator(
                 context = applicationContext,
-                historyRepository = historyRepository,
-                achievementRepository = achievementRepository,
-                notificationsSettingsRepository = notificationsSettingsRepository
-            )
-            achievementEvaluator.evaluate(lastSmokeTime = lastHistory.createdAt, now = now)
+                historyRepository = container.historyRepository,
+                achievementRepository = container.achievementRepository,
+                notificationsSettingsRepository = container.notificationsSettingsRepository
+            ).evaluate(lastSmokeTime = lastHistory.createdAt, now = now)
         }
 
-        val duration: Duration? = lastHistory?.let {
-            Duration.between(it.createdAt, now)
-        }
-
-        Notifications.createNotificationChannel(applicationContext)
-
+        val duration = lastHistory?.let { Duration.between(it.createdAt, now) }
         if (duration != null && notifications?.progress == true) {
-            val frequency: ProgressFrequency = ProgressFrequency.fromValue(settings?.frequency)
-            val intervalMillis: Long = when (frequency) {
-                ProgressFrequency.HOURLY -> Duration.ofHours(1).toMillis()
-                ProgressFrequency.DAILY -> Duration.ofDays(1).toMillis()
-                ProgressFrequency.WEEKLY -> Duration.ofDays(7).toMillis()
-            }
-
-            val lastSentMillis: Long = sharedPreferences.getLong("last_progress_notification_time", 0L)
-            val nowMillis: Long = System.currentTimeMillis()
-
-            val shouldSend: Boolean = lastSentMillis == 0L || (nowMillis - lastSentMillis >= intervalMillis)
-
-            if (shouldSend && duration.toHours() > 1) {
-                Notifications.sendNotification(
-                    context = applicationContext,
-                    title = applicationContext.getString(R.string.notification_title),
-                    content = applicationContext.getString(
-                        R.string.notification_content,
-                        TimeHelper.formatDuration(
-                            resources = applicationContext.resources,
-                            duration = duration
-                        )
-                    ),
-                    notificationId = 1001
-                )
-
-                sharedPreferences.edit {
-                    putLong("last_progress_notification_time", nowMillis)
-                }
-            }
+            checkAndSendProgressNotification(duration, settings?.frequency)
         }
         return Result.success()
+    }
+
+    private fun checkAndSendProgressNotification(duration: Duration, frequency: Int?) {
+        val frequency = ProgressFrequency.fromValue(frequency)
+        val intervalMillis = when (frequency) {
+            ProgressFrequency.HOURLY -> Duration.ofHours(1).toMillis()
+            ProgressFrequency.DAILY -> Duration.ofDays(1).toMillis()
+            ProgressFrequency.WEEKLY -> Duration.ofDays(7).toMillis()
+        }
+
+        val lastSentMillis = sharedPreferences.getLong("last_progress_notification_time", 0L)
+        val nowMillis = System.currentTimeMillis()
+
+        val shouldSend = lastSentMillis == 0L || (nowMillis - lastSentMillis >= intervalMillis)
+        if (shouldSend && duration.toHours() > 1) {
+            Notifications.createNotificationChannel(applicationContext)
+            Notifications.sendNotification(
+                context = applicationContext,
+                title = applicationContext.getString(R.string.notification_title),
+                content = applicationContext.getString(
+                    R.string.notification_content,
+                    TimeHelper.formatDuration(resources = applicationContext.resources, duration = duration)
+                ),
+                notificationId = 1001
+            )
+            sharedPreferences.edit { putLong("last_progress_notification_time", nowMillis) }
+        }
     }
 }
